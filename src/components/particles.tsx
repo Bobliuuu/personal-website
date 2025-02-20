a6ba4af1d237a41a1a1313bb4feb09a5474667b9
Bobliuuu <mmmzzz66g@gmail.com>
2025-02-19 22:14:17 -0500

    Change particles back

--- src/components/particles.tsx
+++ src/components/particles.tsx
@@ -58,7 +58,7 @@ function ParticlesBackground() {
           default: "bounce",
         },
         random: false,
-        speed: 3,
+        speed: 2,
         straight: false,
       },
       number: {
@@ -66,7 +66,7 @@ function ParticlesBackground() {
           enable: true,
           valueArea: 900,
         },
-        value: 80,
+        value: 20,
       },
       opacity: {
         value: 0.3,
